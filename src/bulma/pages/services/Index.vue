3574a90419a39086de0e065e1e0ce57bd371367d
mishu <mishu@xtelecom.ro>
2026-04-10 17:55:14 +0300

    fix: keep service filters local to consuming apps

--- src/bulma/pages/services/Index.vue
+++ src/bulma/pages/services/Index.vue
@@ -1,40 +1,14 @@
 <template>
-    <div class="wrapper">
-        <div class="columns is-centered is-multiline">
-            <div class="column is-narrow">
-                <boolean-filter class="box"
-                    :name="i18n('Active')"
-                    v-model="filters.services.is_active"/>
-            </div>
-            <div class="column is-narrow">
-                <boolean-filter class="box"
-                    :name="i18n('Additional')"
-                    v-model="filters.services.is_additional"/>
-            </div>
-            <div class="column is-narrow">
-                <boolean-filter class="box"
-                    :name="i18n('Contractor Exclusive')"
-                    v-model="filters.services.is_contractor_exclusive"/>
-            </div>
-        </div>
-        <enso-table class="box p-0"
-            v-model:filters="filters"
-            :filter-version="1.1"
-            id="services"/>
-    </div>
+    <enso-table class="box p-0"
+        id="services"/>
 </template>
 
-<script setup>
-import { ref, inject } from 'vue';
-import { BooleanFilter } from '@enso-ui/filters/bulma';
+<script>
 import { EnsoTable } from '@enso-ui/tables/bulma';
 
-const i18n = inject('i18n');
-const filters = ref({
-    services: {
-        is_active: null,
-        is_additional: null,
-        is_contractor_exclusive: null,
-    },
-});
+export default {
+    name: 'Index',
+
+    components: { EnsoTable },
+};
 </script>
